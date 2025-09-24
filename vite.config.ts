0674529f27e82a8c273e35eef84d2e05e8055708
Neto test <nikolajeliseev834@gmail.com>
2025-09-24 21:19:34 +0300

    Добавлены пути в vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Money-Converter-App/',
   test: {
     globals: true,
     environment: 'jsdom',
